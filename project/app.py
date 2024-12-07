d0826636b0de9e0e81083a12e1f8cb58ad478998
Vedanth R Kumar <129525647+vedanth2806@users.noreply.github.com>
2024-12-07 14:38:41 +0000

    activity :Flask

--- project/app.py
+++ project/app.py
@@ -1,3 +1,19 @@
 # TODO Build the app.py file along with the template.html file to create a simple To-Do List app
 # The app should have routes for adding a task, and should show a list of tasks
 # Everything you have learned above will be useful for this task
+
+from flask import Flask, render_template;
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/task')
+def task():
+    tasks= ['play football','prepare','study']
+    return render_template('tasks.html', tasks=tasks )
+
+if __name__ == '__main__':
+    app.run(port=8000)
